e549762373b7b8d4bb6f61dca6467cc51e121fb2
Fanwaz <148569656+Fanwaz@users.noreply.github.com>
2025-04-13 09:58:37 -0700

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,8 @@
+/**
+ * Segmented File Upload Progress Component
+ * Copyright (c) 2025 Nuvio Labs
+ * MIT License
+ */
+
+export { default as FileUploadProgress } from "./file-upload-progress"
+export type { FileUploadProgressProps } from "./file-upload-progress"
